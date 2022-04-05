07874cee9a51778348119e0a836891df7117ed33
João Paulo <jp.cm@hotmail.com>
2022-04-05 19:05:27 -0400

    ssl

--- src/database/db.js
+++ src/database/db.js
@@ -3,10 +3,14 @@ import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const connection = new Sequelize(
-    process.env.DATABASE_URL,
-    {
-        dialect: "postgres",
-    });
+export const connection = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
+});
 
 //module.exports = connection;
