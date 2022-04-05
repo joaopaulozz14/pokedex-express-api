e2fbc227c78fba5e6167e77245bb341304ab4ffe
João Paulo <jp.cm@hotmail.com>
2022-04-04 21:40:17 -0400

    alteracoes

--- src/controllers/PokemonController.js
+++ src/controllers/PokemonController.js
@@ -1,35 +1,86 @@
 //const Pokemon = require('../models/Pokemon');
 //import express from 'express'
 //import { redirect } from 'express/lib/response';
-import { Pokemon } from '../models/Pokemon.js'
+import { Pokemon } from '../models/Pokemon.js';
+const orderById = {order: [["id", "ASC"]]};
 
 export const getAll = async (req, res) => {
-    try{
-        const pokedex = await Pokemon.findAll();
-        res.render("index", {pokedex, pokemon: undefined});
-    } catch (err){
-        res.status(500).send({err: err.message});
+    try {
+        const pokedex = await Pokemon.findAll(orderById);
+        res.render("index", { pokedex, pokemonPut: null, pokemonDel: null});
+    } catch (err) {
+        res.status(500).send({ err: err.message });
     }
 };
 
-export const signup =  (req,res) => {
-    try{
+export const signup = (req, res) => {
+    try {
         res.render("signup")
-    } catch (err){
-        res.status(500).send({err: err.message});
+    } catch (err) {
+        res.status(500).send({ err: err.message });
     }
-}
+};
 export const create = async (req, res) => {
-    try{
+    try {
         const pokemon = req.body;
-        if(!pokemon){
+        if (!pokemon) {
             return res.redirect("/signup");
         }
         await Pokemon.create(pokemon);
+        res.redirect("/");
+    } catch (err) {
+        res.status(500).send({ err: err.message });
+    }
+};
+
+export const getById = async (req, res) => {
+    try {
+        const method = req.params.method;
+        const pokedex = await Pokemon.findAll(orderById);
+        const pokemon = await Pokemon.findByPk(req.params.id);
+
+        if (method === 'put') {
+            res.render("index", {
+                pokedex,
+                pokemonPut: pokemon,
+                pokemonDel: null
+            });
+        } else {
+            res.render("index", {
+                pokedex,
+                pokemonPut: null,
+                pokemonDel: pokemon
+            });
+
+        }
+    } catch (err) {
+        res.status(500).send({ err: err.message });
+    }
+}
+
+export const update = async (req,res) => {
+    try{
+        const pokemon = req.body;
+        await Pokemon.update(pokemon, {where: {id: req.params.id}})
         res.redirect("/");
     } catch(err){
         res.status(500).send({err: err.message});
     }
 }
+export const remove = async (req, res) => {
+    try{
+        await Pokemon.destroy({where: {id: req.params.id}});
+    } catch(err){
+        res.status(500).send({err: err.message});
+    }
+}
 
+/**export const personagem = async (req, res) => {
+    try{
+        const pokemon = await Pokemon.findByPk(req.params.id);
+    } catch(err) {
+        res.status(500).send({err: err.message});
+    }
+}
+*/
 //module.exports = { getAll };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,13 @@
 //const routes = require('express').Router();
 import express from 'express';
 //import { route } from 'express/lib/application';
-import { getAll, signup, create } from '../controllers/PokemonController.js'
+import { getAll, signup, create, getById, update, remove } from '../controllers/PokemonController.js'
 export const routes = express.Router();
 //const PokemonController = require('../controllers/PokemonController');
 routes.get("/", getAll);
 routes.get("/signup", signup);
-routes.post("/create", create)
+routes.post("/create", create);
+routes.get("/getById/:id/:method", getById);
+routes.post("/update/:id", update);
+routes.get("/remove/:id", remove)
 //module.exports = routes;
